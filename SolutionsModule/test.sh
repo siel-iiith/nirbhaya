c3bd7e5e4c0dc0f68db794d21b603cc73eee3120
adityamogadala <aditya.mogadala@gmail.com>
2013-03-20 22:22:09 +0530

    added mongodb jar to mvn install

--- SolutionsModule/test.sh
+++ SolutionsModule/test.sh
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=/home/aditya/Dropbox/Nirbhaya/SolutionsModule/lib/mongo-2.10.1.jar -DgroupId=com.mongodb -DartifactId=mongodb-java-driver -Dversion=2.10.1 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=/home/aditya/Dropbox/nirbhaya/SolutionsModule/lib/mongo-2.10.1.jar -DgroupId=com.mongodb -DartifactId=mongodb-java-driver -Dversion=2.10.1 -Dpackaging=jar -DgeneratePom=true
