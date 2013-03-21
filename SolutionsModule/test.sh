5c953be5336c4cdc531de527654dcd416b1ff4ae
adityamogadala <aditya.mogadala@gmail.com>
2013-03-21 11:05:18 +0530

    changed test.sh for path

--- SolutionsModule/test.sh
+++ SolutionsModule/test.sh
@@ -1,2 +1,2 @@
-smpath=/home/aditya/Dropbox/nirbhaya
-mvn install:install-file -Dfile=$smpath/SolutionsModule/WebContent/WEB-INF/lib/mongo-2.10.1.jar -DgroupId=com.mongodb -DartifactId=mongodb-java-driver -Dversion=2.10.1 -Dpackaging=jar -DgeneratePom=true
+smpath=`pwd`
+mvn install:install-file -Dfile=$smpath/WebContent/WEB-INF/lib/mongo-2.10.1.jar -DgroupId=com.mongodb -DartifactId=mongodb-java-driver -Dversion=2.10.1 -Dpackaging=jar -DgeneratePom=true
