e57924618999ebd01d6fd7a3fea7d291d48d81e2
ajay0221 <ajay0221@gmail.com>
2013-03-28 11:00:13 +0530

    My first commit from Ajay's account

--- Search/src/org/nirbhaya/verticalsearch/QueryExpansion.java
+++ Search/src/org/nirbhaya/verticalsearch/QueryExpansion.java
@@ -4,66 +4,83 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.HashSet;
 import java.util.Set;
 
-public class QueryExpansion {
-	
-	public static Set<String> places;
-	public static Set<String> grievanceWords;
-	private BufferedReader bfr;
-	
-	public QueryExpansion() {
-		
-		places = new HashSet<String>();
-		grievanceWords = new HashSet<String>();
-		
-		String nefile = "/home/clia/en_ne.txt";
-		
-		
-		//String nefile = "/en_ne.txt";
-		String line = "";
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 
-		try {
-			bfr = new BufferedReader(new FileReader(nefile));
-			while ( (line = bfr.readLine()) != null ) {
-				places.add(line.toString().trim().toLowerCase());
-			}
-			
-			nefile = "/home/clia/GrievanceWords.txt";
-			//nefile="GrievanceWords.txt";
-			bfr = new BufferedReader(new FileReader(nefile));
-			line = "";
-			while ( (line = bfr.readLine()) != null ) 
-			{
-				grievanceWords.add(line.toString().trim().toLowerCase());
-			}
-			System.out.println("Loading NEs");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+public class QueryExpansion 
+{
+	/*
+	public static void main(String[] args) throws Exception
+	{
+		QueryExpansion qe = new QueryExpansion();
+		System.out.println(qe.expandedQuery("gachibowli"));
+	}*/
 	
-	public String expandedQuery (String query) {
+    public String expandedQuery (String query) throws UnknownHostException 
+	{
 		String result = query;
-		
 		String[] temp = query.split(" ");
 		
 		boolean placeFlag = false;
 		boolean grievanceFlag = false;
 		
-		for (String s : temp) {
-			if ( grievanceWords.contains(s) ) {
-				grievanceFlag = true;
-			}
-			if ( places.contains(s) ) {
-				placeFlag = true;
-			}
+		// Some MONGO Stuff
+		MongoClient mongoClient = new MongoClient( "10.2.4.180" , 27017 );
+		DB db = mongoClient.getDB( "nirbhaya" );
+		Gson gson = new Gson();
+
+		DBCollection coll = db.getCollection("queryexpansionPlaces");
+		DBCollection coll2 = db.getCollection("queryexpansionGrievances");
+		
+		for (String s : temp) 
+		{
+			BasicDBObject mongoQuery = new BasicDBObject("place", s);
+			DBCursor cursor = coll.find(mongoQuery);
+			
+			try 
+			{
+				   while(cursor.hasNext()) 
+				   {
+					   DBObject d = cursor.next();
+					   Place place = gson.fromJson(d.toString(),Place.class);
+					   placeFlag=true;
+				   }
+				   // System.out.println(placeFlag);
+			} 
+			
+			finally 
+			{
+				   cursor.close();
+		    }
+			
+			
+			mongoQuery = new BasicDBObject("grievance", s);
+			cursor = coll2.find(mongoQuery);
+			
+			try 
+			{
+				   while(cursor.hasNext()) 
+				   {
+					   DBObject d = cursor.next();
+					   Place place = gson.fromJson(d.toString(),Place.class);
+					   grievanceFlag=true;
+				   }
+			} 
+			
+			finally 
+			{
+				   cursor.close();
+		    }
+			
 		}
 		
 		if ( grievanceFlag == false ) { //Pick top trending grievance words
