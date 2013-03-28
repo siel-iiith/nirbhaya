c7ca74720e46874962edc604517f6b79a2bbfcbf
harshi3t <harricot11@gmail.com>
2013-03-28 12:31:11 +0530

    Query Processing Started

--- SolutionsModule/test.sh
+++
@@ -1,2 +0,0 @@
-smpath=`pwd`
-mvn install:install-file -Dfile=$smpath/WebContent/WEB-INF/lib/mongo-2.10.1.jar -DgroupId=com.mongodb -DartifactId=mongodb-java-driver -Dversion=2.10.1 -Dpackaging=jar -DgeneratePom=true
