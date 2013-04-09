9c0a755e69cd3b1ec78436c2d7f52e604db8235a
harshi3t <harricot11@gmail.com>
2013-04-10 01:40:35 +0530

    pom.xml updated for Stemmer jar

--- SolutionsModule/test.sh
+++ SolutionsModule/test.sh
@@ -1,2 +1,3 @@
-smpath=/home/aditya/Dropbox/nirbhaya
+smpath=`pwd`
 mvn install:install-file -Dfile=$smpath/SolutionsModule/WebContent/WEB-INF/lib/mongo-2.10.1.jar -DgroupId=com.mongodb -DartifactId=mongodb-java-driver -Dversion=2.10.1 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=$smpath/SolutionsModule/WebContent/WEB-INF/lib/snowball-20051019.jar -DgroupId=org.tartarus.snowball.ext -DartifactId=english-stemmer -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
