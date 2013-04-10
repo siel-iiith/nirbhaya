9aed53d67e089d07ec1f25f8954cfb7dbf5694a9
harshi3t <harricot11@gmail.com>
2013-04-10 20:04:34 +0530

    pom.xml updated

--- SolutionsModule/test.sh
+++ SolutionsModule/test.sh
@@ -1,3 +1,4 @@
+wget http://weka.wikispaces.com/file/view/snowball-20051019.jar/82917267/snowball-20051019.jar
 smpath=`pwd`
-mvn install:install-file -Dfile=$smpath/SolutionsModule/WebContent/WEB-INF/lib/mongo-2.10.1.jar -DgroupId=com.mongodb -DartifactId=mongodb-java-driver -Dversion=2.10.1 -Dpackaging=jar -DgeneratePom=true
-mvn install:install-file -Dfile=$smpath/SolutionsModule/WebContent/WEB-INF/lib/snowball-20051019.jar -DgroupId=org.tartarus.snowball.ext -DartifactId=english-stemmer -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=$smpath/WebContent/WEB-INF/lib/mongo-2.10.1.jar -DgroupId=com.mongodb -DartifactId=mongodb-java-driver -Dversion=2.10.1 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=$smpath/WebContent/WEB-INF/lib/snowball-20051019.jar -DgroupId=org.tartarus.snowball.ext -DartifactId=english-stemmer -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
